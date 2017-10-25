1622adc06b345cda659f87c729fd580c9142d509
dskrenta <dskrenta@gmail.com>
2017-10-24 19:03:19 -0600

    added robot class

--- recitation9/robot.cpp
+++ recitation9/robot.cpp
@@ -1,13 +1,63 @@
 #include <iostream>
 using namespace std;
 
-class robot {
+class Robot {
     public:
-        Robot() {
-
+        Robot(int start_x, int start_y, char start_heading) {
+            x = start_x;
+            y = start_y;
+            heading = start_heading;
         }
 
-        ~Robot() {
-
+        int getX() {
+            return x;
         }
-}
+
+        int getY() {
+            return y;
+        }
+
+        char getHeading() {
+            return heading;
+        }
+
+        void setX(int new_x) {
+            x = new_x;
+        }
+
+        void setY(int new_y) {
+            y = new_y;
+        }
+
+        void setHeading(char new_heading) {
+            heading = new_heading;
+        }
+
+        void turnLeft() {
+            if (heading == 'N') {
+                heading = 'W';
+            }
+        }
+
+        void turnRight() {
+            if (heading == 'N') {
+                heading = 'E';
+            }
+            else if (heading == 'E') {
+                heading = 'S';
+            }
+        }
+
+        void moveForward() {
+            if (heading == 'N') {
+                y++;
+            }
+            else if (heading == 'S') {
+                y--;
+            }
+        }
+
+        int x;
+        int y;
+        char heading;
+};
