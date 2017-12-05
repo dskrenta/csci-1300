306a56dd18093cfc482008d4ad719359bc6d37d4
dskrenta <dskrenta@gmail.com>
2017-12-04 22:48:42 -0800

    avg rating work

--- assignment10/assignment10.py
+++ assignment10/assignment10.py
@@ -24,13 +24,32 @@ class Recommender:
         
         for line in file:
             values = line.rstrip().split(' ')
-            self.users[values[0]] = values[1:]
+            self.users[values[0]] = map(lambda rating: int(rating), values[1:])
+            
+    def calc_avg_rating(self):
+        self.avg_ratings = []
+        for value in self.users.iteritems():
+            sum = 0.0
+            count = 0
+            for rating in value[1]:
+                if rating > 0:
+                    sum += rating
+                    count += 1
+            self.avg_ratings.append(sum / count)    
+    
+    def lookup_avg_rating(self, index):
+        return '(' + str(self.avg_ratings[index]) + ')' + ' ' + self.books[index][0] + ' by ' + self.books[index][1]
         
 def main():
     recommender = Recommender('book.txt', 'ratings.txt')
     
-    print recommender.books
-    print recommender.users 
+    # print recommender.books
+    # print recommender.users
+    
+    recommender.calc_avg_rating() 
+    
+    # print recommender.avg_ratings
+    print recommender.lookup_avg_rating(5)
 
 if __name__ == '__main__':
     main()
